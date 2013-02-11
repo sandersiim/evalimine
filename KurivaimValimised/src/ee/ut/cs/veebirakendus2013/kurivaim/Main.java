8383137438ba9662b15791dedd402054da2301aa
Sander Siim <sander.siim1@gmail.com>
2013-02-11 15:46:24 +0200

    first commit

---
+++ KurivaimValimised/src/ee/ut/cs/veebirakendus2013/kurivaim/Main.java
@@ -0,0 +1,14 @@
+package ee.ut.cs.veebirakendus2013.kurivaim;
+
+
+public class Main {
+	
+	/**
+	 * @param args
+	 */
+	public static void main( String[] args ) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
