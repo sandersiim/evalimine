07781dce1daf661a5ead8a7445928025f06ff053
Ago Allikmaa <maxorator@gmail.com>
2013-02-14 08:56:51 +0200

    Fixed a typo in SQL import file.

--- sqlimport/import_dbanduser.sql
+++ sqlimport/import_dbanduser.sql
@@ -1,4 +1,4 @@
-CREATE USER 'evalimised_user'@'localhost' IDENTIFIED BY 'PaSsWoRd123'
+CREATE USER 'evalimised_user'@'localhost' IDENTIFIED BY 'PaSsWoRd123';
 
 CREATE DATABASE `evalimised` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 GRANT ALL PRIVILEGES ON evalimised.* TO 'evalimised_user'@'localhost' WITH GRANT OPTION;
