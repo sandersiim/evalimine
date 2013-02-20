2c5af5515d2bcaba2726f284ded2e30b0510a3a2
Ago Allikmaa <maxorator@gmail.com>
2013-02-20 22:31:40 +0200

    Updated JS tab system, attached login

--- html/test.js
+++ html/test.js
@@ -1,64 +1,120 @@
-var loggedIn = false;
+var voteSystem = {}
 
-function resizeElements() {
+voteSystem.loggedIn = false;
+voteSystem.activeMenuItem = "menu_mydata"
+voteSystem.menuActiveTab = {"menu_help" : "tab_help", "menu_mydata" : "tab_login", "menu_statistics" : "tab_stats_regions", "menu_voting" : "tab_voting"};
+
+voteSystem.resizeElements = function() {
 	$("#mainblock").height($(window).height()-($("#mainblock").outerHeight(true)-$("#mainblock").height())); 
 	$("#content").height($("#mainblock").height()-$("#header").outerHeight()-$("#footer").outerHeight()); 
-}
+};
 
-function addClassToElement(element,clazz) {
-	if (element.className.indexOf(clazz) == -1) {
-		element.className += " "+clazz;
+voteSystem.addClassToElement = function(element, oneClass) {
+	if (element.className.indexOf(oneClass) == -1) {
+		element.className += " " + oneClass;
 	}
+};
+
+voteSystem.removeClassFromElement = function(element, oneClass) {
+	element.className = element.className.replace(oneClass, "");
+};
+
+voteSystem.changeActiveMenuClass = function(newMenuSelection) {
+	$.each($(".menuitem.menuselected"), function(index, oldMenuItem) {
+		voteSystem.removeClassFromElement(oldMenuItem, "menuselected");
+	});
+	
+	voteSystem.addClassToElement(newMenuSelection, "menuselected");
+};
+
+voteSystem.setActiveMenuItem = function(newMenuSelection) {
+	voteSystem.changeActiveMenuClass(newMenuSelection);
+	voteSystem.swapToActiveTabForMenuItem(newMenuSelection)
+};
+
+voteSystem.swapToActiveTabForMenuItem = function(selectedMenu) {
+	if(voteSystem.menuActiveTab[selectedMenu.id]) {
+		voteSystem.swapToTab(voteSystem.menuActiveTab[selectedMenu.id]);
+	}
+};
+
+voteSystem.setActiveTabForMenu = function(menuItemName, tabName, activateMenu) {
+	if(voteSystem.menuActiveTab[menuItemName]) {
+		voteSystem.menuActiveTab[menuItemName] = tabName;
+	}
+
+	if(this.activeMenuItem == menuItemName || activateMenu) {
+		voteSystem.setActiveMenuItem($("#" + menuItemName)[0]);
+	}
+};
+
+voteSystem.swapToTab = function(tabElement) {
+	$.each($(".tab.visible"), function(index, oldTab) {
+		voteSystem.removeClassFromElement(oldTab, "visible");
+	});
+	
+	$.each($("#" + tabElement), function(index, newTab) {
+		voteSystem.addClassToElement(newTab, "visible");
+	});
+};
+
+voteSystem.jsonQuery = function(queryType, jsonObject, isPostQuery, doneFunction) {
+	$.ajax("dyn/" + queryType + "?json=" + JSON.stringify(jsonObject), {type: isPostQuery ? "POST" : "GET", dataType: "json"}).done(doneFunction);
+};
+
+voteSystem.setLoggedInStatus = function(isLoggedIn) {
+	if(isLoggedIn) {
+		$("#menu_mydata").text("Minu andmed");
+		voteSystem.setActiveTabForMenu("menu_mydata", "tab_mydata", false);
+	}
+	else {
+		$("#menu_mydata").text("Logi sisse");
+		voteSystem.setActiveTabForMenu("menu_mydata", "tab_login", false);
+	}
+	
+	voteSystem.loggedIn = isLoggedIn;
 }
 
-function removeClassFromElement(element,clazz) {
-	element.className = element.className.replace(clazz,"");
+voteSystem.queryStatus = function() {
+	voteSystem.jsonQuery("status", {}, false, function(data) {
+		if(data.responseType == "userInfo" && data.userInfo != null) {
+			voteSystem.setLoggedInStatus(true);
+		}
+	});
 }
 
-function changeMenuSelection(newMenuSelection) {
-	var oldMenuSelection = $(".menuitem.menuselected")[0];
-	removeClassFromElement(oldMenuSelection, "menuselected");
-	addClassToElement(newMenuSelection,"menuselected")
-}
+voteSystem.initialise = function() {
+	$(".menuitem").click( function() {
+		voteSystem.setActiveMenuItem(this);
+	});
 
-function swapTab(selectedMenu) {
-	var oldTab = $(".tab.visible")[0];
-	removeClassFromElement(oldTab,"visible");	
-	switch(selectedMenu.id) {
-		case "menu_help":
-			addClassToElement($("#tab_help")[0],"visible");
-			break;
-		case "menu_mydata":
-			if(loggedIn) {
-				addClassToElement($("#tab_mydata")[0],"visible");
-			} else {
-				addClassToElement($("#tab_login")[0],"visible");
+	$("#loginByPassword").submit( function() {
+		voteSystem.jsonQuery("login", {username:$("#username").val(), password:$("#password").val()}, false, function(data) {
+			if(data.responseType == "userInfo" && data.userInfo != null) {
+				voteSystem.setLoggedInStatus(true);
 			}
-			break;
-		case "menu_statistics":
-			addClassToElement($("#tab_statistics")[0],"visible");
-			break;
-		case "menu_voting":
-			addClassToElement($("#tab_voting")[0],"visible");
-			break;
-
-	}
-}
+			else if(data.responseType == "status") {
+				if(data.statusCode < 0) {
+					$("#loginErrorMessage").text("Süsteemi viga.");
+				}
+				else if(data.statusCode == 1) {
+					$("#loginErrorMessage").text("Vale kasutajanimi/parool.");
+				}
+			}
+		});
+		
+		return false;
+	});
+	
+	voteSystem.resizeElements();
+	
+	$(window).resize(function() {
+		voteSystem.resizeElements();
+	});
+	
+	voteSystem.queryStatus();
+};
 
 $(document).ready(function() {
-	$(".menuitem").click( function() {
-		changeMenuSelection(this);
-		swapTab(this);
-	});
-
-	$("#loginbutton").click( function() {
-		$("#menu_mydata").html("Minu andmed");
-		loggedIn = true;
-		swapTab($("#menu_mydata")[0]);
-	});
-		
-	resizeElements();
-	$(window).resize(function() {
-		resizeElements();
-	});
+	voteSystem.initialise();
 });
